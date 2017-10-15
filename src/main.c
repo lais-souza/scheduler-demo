a6435e380acbce443320ec7a75fa66d5aa75ceaf
lais-souza <la.oliveiradesouza@gmail.com>
2017-10-15 16:50:18 -0200

    Sem teste e programa incompleto, para primeiros testes

--- src/main.c
+++ src/main.c
@@ -3,13 +3,33 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
+  int N[5];
   int n;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+  scanf("%d %d %d %d %d %d", &(N[0]), &(N[1]), &(N[2]), &(N[3]), &(N[4]), &n);
 
   for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  	if ((i+1) == N[0]){
+  		printf("0");
+  		N[0] = N[0] + N[0];
+	  }
+	  if ((i+1) == N[1]){
+  		printf("1");
+  		N[1] = N[1] + N[1];
+	  }
+	  if ((i+1) == N[2]){
+  		printf("2");
+  		N[2] = N[2] + N[2];
+	  }
+	  if ((i+1) == N[3]){
+  		printf("3");
+  		N[3] = N[3] + N[3];
+	  }
+	  if ((i+1) == N[4]){
+  		printf("4");
+  		N[4] = N[4] + N[4];
+	  }
+	  printf("'\n'\n");
   }
 
   return 0;
